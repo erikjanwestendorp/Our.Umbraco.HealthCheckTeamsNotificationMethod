88a63af9c576aca0ece794b7b654ae680b72377c
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-06-23 12:59:21 +0200

    separate projects

---
+++ src/Our.Umbraco.HealthCheckTeamsNotificationMethod/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Our.Umbraco.HealthCheckTeamsNotificationMethod;
+
+public static class Constants
+{
+    public const string Message = "message";
+    public const string ImageUrl = "https://umbraco.com/media/ziikdjap/umbraco_social_og.png";
+
+    public static class NotificationMethods
+    {
+        public const string Teams = "teams";
+    }
+}
